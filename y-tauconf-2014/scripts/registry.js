87baa556c47a398cfeb1aa27c4e5397163a276d8
Uladzimir Abramchuk <u.abramchuk@gmail.com>
2014-05-07 21:16:02 +0300

    tauconf-6: intron added

--- y-tauconf-2014/scripts/registry.js
+++ y-tauconf-2014/scripts/registry.js
@@ -218,7 +218,8 @@ window.shower.registry = {
 			handler: function() {
 				window.quine = function() {
 var newLine = String.fromCharCode(10);
-var data = 'var newLine = String.fromCharCode(10);%3var data = %1%0%1;%3console.log(data.replace(/%21/g, "%1").replace(/%23/g, newLine).replace(/%22/g, "%").replace("%20", data));';
+//intron
+var data = 'var newLine = String.fromCharCode(10);%3//intron%3%3var data = %1%0%1;%3console.log(data.replace(/%21/g, "%1").replace(/%23/g, newLine).replace(/%22/g, "%").replace("%20", data));';
 console.log(data.replace(/%1/g, "'").replace(/%3/g, newLine).replace(/%2/g, "%").replace("%0", data));					
 				};
 			}
