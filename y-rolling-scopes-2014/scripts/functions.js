bc976642eeb60d3f1b748fc008736d21fe61ccfd
Uladzimir Abramchuk <u.abramchuk@gmail.com>
2014-08-07 12:44:02 +0300

    scripts fixed

--- y-rolling-scopes-2014/scripts/functions.js
+++ y-rolling-scopes-2014/scripts/functions.js
@@ -1,36 +1,34 @@
-if (!window.shower) {
-	return;
-}
+if (window.shower) {
+	(function(window) {
+		window.shower.registry = {};
 
-(function(window) {
-	window.shower.registry = {};
+		var runScript = function(slideHash) {
+			var script = window.shower.registry[slideHash];
 
-	var runScript = function(slideHash) {
-		var script = window.shower.registry[slideHash];
-
-		if (script && script.handler) {
-			if (script.dependencies) {
-				for(var i = 0; i < script.dependencies.length; i++) {
-					runScript(script.dependencies[i]);
+			if (script && script.handler) {
+				if (script.dependencies) {
+					for(var i = 0; i < script.dependencies.length; i++) {
+						runScript(script.dependencies[i]);
+					}
 				}
+				script.handler();
 			}
-			script.handler();
+		};
+
+		var originalGo = window.shower.go;
+
+		var newGo = function(slideNumber, callback) {
+			var result = originalGo(slideNumber, callback);
+
+			if (window.shower.isSlideMode()) {
+				var slideHash = window.shower.getSlideHash(slideNumber).substr(1);	
+
+				runScript(slideHash);
+			}
+
+			return result;
 		}
-	};
 
-	var originalGo = window.shower.go;
-
-	var newGo = function(slideNumber, callback) {
-		var result = originalGo(slideNumber, callback);
-
-		if (window.shower.isSlideMode()) {
-			var slideHash = window.shower.getSlideHash(slideNumber).substr(1);	
-
-			runScript(slideHash);
-		}
-
-		return result;
-	}
-
-	window.shower.go = newGo.bind(window.shower);
-})(window);
+		window.shower.go = newGo.bind(window.shower);
+	})(window);
+}
